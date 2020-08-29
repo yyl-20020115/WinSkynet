3e21e1623f7969b498c743594d24290c9015857c
john2015 <yyl_20050115@hotmail.com>
2020-08-29 18:44:05 +0800

    changes on ctrl+c

--- skynet-src/entry.c
+++ skynet-src/entry.c
@@ -10,29 +10,14 @@
 extern int skynet_main(int argc, char* argv[]);
 extern void handle_int(int signal);
 
-BOOL ctrlhandler(DWORD fdwctrltype)
+BOOL WINAPI ctrlhandler(DWORD fdwctrltype)
 {
 	switch (fdwctrltype)
 	{
-		// handle the ctrl-c signal.
 	case CTRL_C_EVENT:
 		printf("Quting skynet...\n\n");
 		handle_int(SIGINT);
-		return(TRUE);
-		// ctrl-close: confirm that the user wants to exit.
-		//case CTRL_CLOSE_EVENT:
-		//	printf("ctrl-close event\n\n");
-		//	return(true);
-		//	// pass other signals to the next handler.
-		//case CTRL_BREAK_EVENT:
-		//	printf("ctrl-break event\n\n");
-		//	return false;
-		//case CTRL_LOGOFF_EVENT:
-		//	printf("ctrl-logoff event\n\n");
-		//	return false;
-		//case CTRL_SHUTDOWN_EVENT:
-		//	printf("ctrl-shutdown event\n\n");
-		//	return false;
+		return TRUE;
 	default:
 		return FALSE;
 	}
@@ -40,7 +25,7 @@ BOOL ctrlhandler(DWORD fdwctrltype)
 
 int main(int argc, char* argv[])
 {
-	SetConsoleCtrlHandler((PHANDLER_ROUTINE)ctrlhandler, TRUE);
+	SetConsoleCtrlHandler(ctrlhandler, TRUE);
 
     return skynet_main(argc, argv);
 }
