90c8637f166bd60216ee17302483443de62963ee
YILIN <YILIN@DESKTOP-YILIN>
2022-05-28 17:46:34 +0800

    mimalloc reused

--- skynet-src/malloc_hook.c
+++ skynet-src/malloc_hook.c
@@ -330,19 +330,19 @@ skynet_strdup(const char *str) {
 #ifdef _WIN32
 void* skynet_malloc(size_t sz)
 {
-	return malloc(sz);
+	return mi_malloc(sz);
 }
 void* skynet_calloc(size_t nmemb, size_t size)
 {
-	return calloc(nmemb, size);
+	return mi_calloc(nmemb, size);
 }
 void* skynet_realloc(void* ptr, size_t size)
 {
-	return realloc(ptr, size);
+	return mi_realloc(ptr, size);
 }
 void skynet_free(void* ptr)
 {
-	free(ptr);
+	mi_free(ptr);
 }
 //void* skynet_memalign(size_t alignment, size_t size)
 //{
